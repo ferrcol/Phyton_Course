3d9ec5c9ccabaebe12bcd965ae459937442fbcab
ferrcol <colcombetfernando@gmail.com>
2025-04-09 23:40:49 +0200

    practice 3

---
+++ 03Areas.py
@@ -0,0 +1,9 @@
+# Rectangle area and perimeter calculator
+
+height = float(input("Enter the height value: "))
+width = float(input("Enter the width value: "))
+
+area = height * width
+perimeter = 2 * (height + width)
+
+print(f'Rectangle area: {area}. Peri2meter: {perimeter}')
